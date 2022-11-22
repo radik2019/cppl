951fa2312d6e158d102fdbe3565b599ead22650f
Radu <rdceb8@gmail.com>
2022-11-22 17:17:12 +0100

    [ADD] added write obj on file

--- file_manager.cpp
+++ file_manager.cpp
@@ -1,12 +1,57 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+/*
+ifstream - modalita read
+ofstream = modalita write
+*/
 
-void write(string file, string data)
+class Point
+{
+private:
+    int x;
+    int y;
+
+public:
+    Point(int x, int y);
+    Point();
+    ~Point();
+    void info()
+    {
+        cout << "x:  " << this->x << endl;
+        cout << "y:  " << this->y << endl;
+    }
+    void changeData(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    
+};
+
+Point::Point()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+Point::Point(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Point::~Point()
+{
+}
+
+void writeDataToFile(const string file, string data)
 {
     ofstream input_file;
     input_file.open(file);
+    // input_file.app;
     if (input_file.is_open())
     {
         input_file << data << endl;
@@ -21,6 +66,7 @@ string read(string file)
 
     // Read from the text file
     ifstream MyReadFile(file);
+
     if (MyReadFile.is_open())
     {
         string temp;
@@ -40,8 +86,87 @@ string read(string file)
     return "";
 }
 
+bool writeObjectToFile(const string path, Point &pt ){
+
+    ofstream wr_file;
+
+    // string path = "my_object_file.dat";
+
+    // si apre il file
+    // path = percorso del file
+    // fstream::app = modalita append
+    wr_file.open(path, fstream::app);
+    if (wr_file.is_open())
+    {
+        wr_file.write((char *)&pt, sizeof(Point));
+        cout << "\nWrited! \n";
+        wr_file.close();
+        return true;
+    }
+    cout << "\nSomething wrong!\n";
+    return false;
+}
+
+vector<Point> readObjectToFile(const string path){
+    ifstream r_file;
+    vector<Point> lst;
+
+    // string path = "my_object_file.dat";
+
+    // si apre il file
+    // path = percorso del file
+    // fstream::app = modalita append
+    r_file.open(path);
+    Point pt;
+    if (r_file.is_open())
+    {
+        //r_file.read((char *)&pt, sizeof(Point)) // il metodo ritorna un bool
+        // se c'e` un dato nelliterazione, ritorna true
+        // altrimenti ritorna false
+        while (r_file.read((char *)&pt, sizeof(Point)))
+        {   
+            lst.push_back(pt);
+        }
+        return lst;
+    }
+    return lst;
+}
+
+
 int main()
 {
-    cout << read("file.cpp") << endl;
+    // Point pt(2, 3);
+    // pt.info();
+    // cout << read("fissle.cpp") << endl;
+
+    //                          SCRITTURA DI UN OGETTO SU UN FILE
+    // Creazione dell'ogetto Point
+    Point pt(211122, 888873);
+
+    // creazione dell'ogetto scrittura su file "ofstream"
+    ofstream wr_file;
+
+    string path = "my_object_file.dat";
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     pt.changeData((i + 1) * (rand() % 10), (i + 34) * (rand() % 10));
+    //     writeObjectToFile(path, pt);
+    // }
+    vector<Point> lst = readObjectToFile(path);
+    lst[45].info();
+    // si apre il file
+    // path = percorso del file
+    // fstream::app = modalita append
+    // wr_file.open(path, fstream::app);
+    // if (wr_file.is_open())
+    // {
+    //     wr_file.write((char *)&pt, sizeof(Point));
+    //     cout << "\nWrited! \n";
+    // }
+    // else
+    // {
+    //     cout << "\nSomething wrong!\n";
+    // }
+
     return 0;
 }
