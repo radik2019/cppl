ce9148f318d019b9789f79333456af43d024985f
radik <codefacs@gmail.com>
2022-12-01 21:57:53 +0100

    [FIX]....

--- forwardListR/forward_list.cpp
+++ forwardListR/forward_list.cpp
@@ -34,7 +34,39 @@ public:
         }
         else
         {
+            Node<T> *current = this->head;
+            while (current->pnext != nullptr)
+            {
+                current = current->pnext;
+            }
+            current->pnext = new Node<T>(data);
         }
+        ++this->Size;
+    }
+
+    int getSize()
+    {
+        return this->Size;
+    }
+
+    T &operator[](const int index)
+    {
+        int counter = 0;
+        Node<T> *current = this->head;
+        while (current != nullptr)
+        {
+            if (counter == index)
+            {
+                return current->data;
+            }
+            current = current->pnext;
+            ++counter;
+        }
+        return current->data;
+    }
+
+    void print(){
+        // TODO 
     }
 
     List(/* args */);
@@ -55,5 +87,19 @@ int main()
 {
     List<int> lst;
     lst.pushBack(5);
+    lst.pushBack(6);
+    lst.pushBack(87);
+    lst.pushBack(12);
+    lst.pushBack(619);
+    lst.pushBack(91);
+    lst.print();
+    cout << "[";
+    for (int i = 0; i < lst.getSize() - 1; i++)
+    {
+        cout << lst[i] << ", ";
+    }
+    
+    cout << lst[lst.getSize()-1]<< "]\n";   
+    
     return 0;
 }
